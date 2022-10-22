450186420e15a71d9a9153e8f30007c71159dfde
Flávio Nassim Bittar Filho <flavionassimbittarfilho@gmail.com>
2022-10-21 22:13:21 -0300

    criação de tema + easter-egg

--- src/main.js
+++ src/main.js
@@ -1,6 +1,50 @@
+// captura da pagina toda
+const body = document.querySelector('body')
+
+// Nome do tema
+const nick = document.querySelector('h1')
+
+// captura todos os botões da calculadora
 const keyboard = document.querySelectorAll('button')
+
+// captura a tela 
 const screen = document.querySelector('.screen')
 
+// captura o botão de tema
+const theme = document.querySelector('.theme')
+
+
+// logica para troca de Tema
+theme.addEventListener('change', () => {
+
+    switch (theme.value) {
+        case '1':
+            keyboard.forEach(button => button.classList.add('t1'))
+            body.classList.add('t1')
+            screen.classList.add('t1')
+            theme.classList.add('t1')
+            nick.textContent = 'Love'
+            nick.classList.add('t1')
+            break;
+    
+        default:
+            keyboard.forEach(button => button.classList.remove('t1'))
+            body.classList.remove('t1')
+            screen.classList.remove('t1')
+            theme.classList.remove('t1')
+            nick.textContent = 'Default'
+            nick.classList.remove('t1')
+            break;
+
+    }
+
+})
+
+
+
+
+
+// logica de calculo 
 const special = ['=', 'ca', 'c']
 let calc = ''
 
@@ -19,9 +63,15 @@ keyboard.forEach(button => {
                 screen.value = calc
             }
             else {
-                calc = eval(calc)
-                calc = String(calc)
-                screen.value = calc
+                if(calc == '02/12/2021' && nick.textContent == 'Love') {
+                    console.log('entrou!!!')
+                    screen.value = 'Eu te amo muitooo, chuchuuu <3'
+                }
+                else {
+                    calc = eval(calc)
+                    calc = String(calc)
+                    screen.value = calc
+                }
             }
         }
         else {
